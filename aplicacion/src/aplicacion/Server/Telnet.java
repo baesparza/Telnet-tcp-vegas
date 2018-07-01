a09afeacb8fab01d83e64e77894d2064ebeaf88c
Bruno Esparza <bruno.be81@gmail.com>
2018-06-30 19:39:45 -0500

    feat(telnet): add basic commands

--- aplicacion/src/aplicacion/Server/Telnet.java
+++ aplicacion/src/aplicacion/Server/Telnet.java
@@ -1,22 +1,69 @@
 package aplicacion.Server;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author bruno
  */
 public class Telnet {
 
-    public static String getCommand(final String c) {
-        System.out.println(c);
-        if ("date".equals(c)) {
-            return "fecha";
+    /**
+     * Return a response for a command
+     *
+     * @param c command
+     * @return response
+     */
+    public static String getCommand(String c) {
+        System.out.println(c.contains("date"));
+        if (c.contains("date")) {
+            return Telnet.date();
         }
-        if ("time".equals(c)) {
-            return "Esta fecha";
+        if (c.contains("open")) {
+            return Telnet.open(c);
         }
-        if ("example".equals(c)) {
-            return "alicia txt";
+        return "The command doesn't exist";
+    }
+
+    /**
+     * @return actual date
+     */
+    private static String date() {
+        Date date = new Date();
+        return date.toString();
+    }
+
+    /**
+     * Open file and return its content
+     *
+     * @param c command
+     * @return file content
+     */
+    private static String open(final String c) {
+        String[] args = c.split(" ");
+
+        StringBuilder resp = new StringBuilder();
+        List<String> records = new ArrayList<>();
+        // read file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("./assets/" + args[1]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                records.add(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            return ("Can't read file or file doesn't exist");
         }
-        return "El comando no existe";
+        // return file content
+        for (String r : records) {
+            resp.append(r).append("\n");
+        }
+        return resp.toString();
     }
 }
